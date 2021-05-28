cbcbeb8614876c47c89e63ce19ccd4af5b616bba
Chris Rogers <47615352+chrislrogers@users.noreply.github.com>
2021-05-29 00:03:59 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# fovcalculator
+# Vertical Field of View Calculator
+
+This calculator determines Vertical FoV from a given Horizontal FoV and Resolution (or aspect ratio).
  
+View live [here](https://chrislrogers.github.io/fovcalculator/)
---
+++ script.js
@@ -0,0 +1,24 @@
+const message = document.getElementById('message');
+
+function calculateFOV() {
+    let width = document.getElementById("width").value;
+    let height = document.getElementById("height").value;
+    let horizontalFOV = document.getElementById("horizontalFOV").value;
+    let verticalFOV;
+    if (width !== "" && height !== "" && horizontalFOV !== "") {
+
+
+        // convert to radians
+        horizontalFOV = horizontalFOV * (Math.PI / 180);
+
+        // calculate vertical fov
+        verticalFOV = 2 * Math.atan(Math.tan(horizontalFOV / 2) * (height / width));
+
+        // convert to degrees
+        verticalFOV = Math.round(verticalFOV * (180 / Math.PI));
+
+        message.textContent = "Calculated vertical FOV: " + verticalFOV;
+    } else {
+        message.textContent = "All fields must be filled out.";
+    }
+}
